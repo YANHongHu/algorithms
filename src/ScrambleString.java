13fdadddd656cb6195c50f660b8b765ea075a989
鸿鹄~ <49400532+YANHongHu@users.noreply.github.com>
2021-01-28 21:20:32 +0800

    扰乱字符串（改进）
    使用递归方法

--- src/ScrambleString.java
+++ src/ScrambleString.java
@@ -1,3 +1,4 @@
+
 /**
  * 给定一个字符串 s1，我们可以把它递归地分割成两个非空子字符串，从而将其表示为二叉树。
  * 下图是字符串 s1 = "great" 的一种可能的表示形式。
@@ -44,19 +45,18 @@ public class ScrambleString {
     public static void main(String[] args) {
         String s1 = "great";
         String s2 = "rgeat";
-        System.out.println(isScramble(s1, s2));
+        System.out.println(isScramble1(s1, s2));
+        System.out.println(isScramble2(s1, s2));
     }
 
     /**
-     * 执行用时：
-     * 11 ms
-     * 内存消耗：
-     * 38.6 MB
+     * 执行用时：11 ms
+     * 内存消耗：38.6 MB
      * @param s1
      * @param s2
      * @return
      */
-    public static boolean isScramble(String s1, String s2) {
+    public static boolean isScramble1(String s1, String s2) {
         if(s1.length()!=s2.length()){
             return false;
         }
@@ -82,18 +82,55 @@ public class ScrambleString {
                         if(dp[i][j][k]){
                             break;
                         }
-                        //if (dp[i][j][m]&&dp[i+m][j+m][k-m]){
-                            //dp[i][j][k]=true;
-                            //break;
-                        //}
-                        //if (dp[i][j+k-m][m]&&dp[i+m][j][k-m]){
-                            //dp[i][j][k]=true;
-                            //break;
-                        //}
                     }
                 }
             }
         }
         return dp[0][0][len];
+    }
+
+    /**
+     * 执行用时：2 ms
+     * 内存消耗：38.6 MB
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public static boolean isScramble2(String s1, String s2) {
+        // 如果是空，返回false
+        if (s1 == null || s2 == null) return false;
+        // 如果两个字符串的长度不相等，返回false
+        if (s1.length() != s2.length()) return false;
+        int len = s1.length();
+        // 如果两个字符串的值相等，返回true
+        if (s1.equals(s2)) return true;
+        // 定义一个长度为26的一维数组，下标0-25分别代表了26个字母和字母a的差值，相当于代表了26个字母
+        int[] letters = new int[26];
+        // 循环遍历s1和s2，s1里出现的就给对应的字母+1，s2出现的就给对应的字母-1
+        for (int i = 0; i < len; i++) {
+            letters[s1.charAt(i) - 'a']++;
+            letters[s2.charAt(i) - 'a']--;
+        }
+        // 判断每个字母对应的值是否是0，如果不是，说明s1和s2里的字符不相同，返回false
+        for (int i = 0; i < 26; i++) {
+            if (letters[i] != 0) {
+                return false;
+            }
+        }
+        // 当两个字符串长度小于等于3时，各个字符串拥有的字符都相同就一定能转换
+        if(len<=3){
+            return true;
+        }else {
+            // 对s1和s2划分区间
+            for (int i = 1; i < len; i++) {
+                // 选取s1和s2的前i个字符进行递归比较，并且将两个字符串剩下的字符进行递归比较，如果两个比较的结果都是true则返回true
+                if (isScramble2(s1.substring(0, i), s2.substring(0, i)) && isScramble2(s1.substring(i), s2.substring(i)))
+                    return true;
+                // 选取s1的前i个字符和s2的后i个字符进行递归比较，并且将两个字符串剩下的字符进行递归比较，如果两个比较的结果都是true则返回true
+                if (isScramble2(s1.substring(0, i), s2.substring(len - i)) && isScramble2(s1.substring(i), s2.substring(0, len - i)))
+                    return true;
+            }
+        }
+        return false;
     }
 }
